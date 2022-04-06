451a4dc1e7ae8b6d203acf48e95be7719ff71252
Amanda Reyes <areyes4368@stu.d214.org>
2022-04-06 10:28:50 -0500

    added outlets

--- GitHubPractice2/ViewController.swift
+++ GitHubPractice2/ViewController.swift
@@ -7,13 +7,23 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController , UITableViewDataSource {
 
+    @IBOutlet weak var tableView: UITableView!
+    let animals = ["Wolf","Bear","Bird"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        tableView.dataSource = self
     }
-
-
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return animals.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "myCell", for: indexPath)
+        cell.textLabel?.text = "\(animals[indexPath.row])"
+        return cell
+    }
 }
 
